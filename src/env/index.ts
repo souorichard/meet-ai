65536f806b5aa67a10dbb3f6d7b683a336b0ee04
souorichard <rodrigues.lpta@gmail.com>
2025-11-25 15:48:39 -0300

    feat: add Better Auth setup and push its default auth schema

--- src/env/index.ts
+++ src/env/index.ts
@@ -2,6 +2,9 @@ import z from 'zod'
 
 const envSchema = z.object({
   DATABASE_URL: z.string(),
+
+  BETTER_AUTH_SECRET: z.string(),
+  BETTER_AUTH_URL: z.string(),
 })
 
 export const env = envSchema.parse(process.env)
